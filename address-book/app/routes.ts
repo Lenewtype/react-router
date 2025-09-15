5eed924525b85dfd3ecafd5b05411568135f3504
Nick M <njm5586@g.rit.edu>
2025-09-14 23:40:56 -0400

    Getting further along in the tutorial

--- address-book/app/routes.ts
+++ address-book/app/routes.ts
@@ -1,3 +1,11 @@
-import type { RouteConfig } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
 
-export default [] satisfies RouteConfig;
+export default [
+    layout("layout/sidebar.tsx", [
+        index("routes/home.tsx"),
+        route("contacts/:contactId", "routes/contact.tsx"),
+        route("contacts/:contactId/edit", "routes/edit-contact.tsx"),
+        route("contacts/:contactId/destroy", "routes/destroy-contact.tsx"),
+    ]),
+    route("about", "routes/about.tsx"),
+] satisfies RouteConfig;
